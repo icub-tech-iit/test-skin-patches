98b820c307ae4f23ed33b3ceed8eeac8486ba5df
simeonedussoni <simeone.dussoni@iit.it>
2024-10-02 14:33:14 +0200

    fingertip ft2 test reset taxel script

--- config-files/scripts/reset_tax.sh
+++ config-files/scripts/reset_tax.sh
@@ -2,6 +2,6 @@ cansend can0 20a#4d0164f0
 cansend can0 20a#50000f02012200
 cansend can0 20a#0700
 cansend can0 20a#4d0164f0
-cansend can0 20a#50000f02010000
+cansend can0 20a#50000f00010000
 cansend can0 20a#0700
 
