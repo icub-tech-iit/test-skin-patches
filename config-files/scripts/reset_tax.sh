adfc1790ab319e19f095784324229bd149866ff3
simeonedussoni <simeone.dussoni@iit.it>
2024-10-10 11:12:00 +0200

    ft2 testing script fixing - header and exe permits

--- config-files/scripts/reset_tax.sh
+++ config-files/scripts/reset_tax.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+################################################################################
+#                                                                              #
+# Copyright (C) 2020 Fondazione Istitito Italiano di Tecnologia (IIT)          #
+# All Rights Reserved.                                                         #
+#                                                                              #
+################################################################################
+
+# launch the configuration commands 
+
 cansend can0 20a#4d0164f0
 cansend can0 20a#50000f02012200
 cansend can0 20a#0700
--- config-files/scripts/setcan.sh
+++ config-files/scripts/setcan.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+################################################################################
+#                                                                              #
+# Copyright (C) 2020 Fondazione Istitito Italiano di Tecnologia (IIT)          #
+# All Rights Reserved.                                                         #
+#                                                                              #
+################################################################################
+
+# enable the esd can device
+
 sudo ip link set can0 type can bitrate 1000000
 sudo ip link set up can0
 
